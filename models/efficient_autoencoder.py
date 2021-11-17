2be4b1ba49c37c6d18f59142e53688c6973219ab
Nicholas Capel <nicholas.rj.capel@gmail.com>
2021-11-17 07:45:12 -0800

    Refactor efficient autoencoder

--- models/efficient_autoencoder.py
+++ models/efficient_autoencoder.py
@@ -1,3 +1,6 @@
+import operator
+from functools import reduce
+
 import gin
 import torch
 from torch import nn
@@ -7,12 +10,16 @@ from models.autoencoder import AutoEncoder
 
 @gin.configurable
 class EfficientAutoEncoder(AutoEncoder):
-    def __init__(self, compressed_dim=196, **kwargs):
+    def __init__(self, before_compressed_dim=(4, 7, 7), compressed_dim=10, **kwargs):
         super().__init__()
         self.compressed_dim = compressed_dim
+        self.before_compressed_dim = before_compressed_dim
         # Encoder
         self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
         self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
+        self.num_before_compressed_dim = reduce(operator.mul, before_compressed_dim, 1)
+        self.linear_encoder = nn.Linear(self.num_before_compressed_dim, compressed_dim)
+        self.linear_decoder = nn.Linear(compressed_dim, self.num_before_compressed_dim)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
         # Decoder
@@ -20,17 +27,23 @@ class EfficientAutoEncoder(AutoEncoder):
         self.t_conv2 = nn.ConvTranspose2d(16, 1, 2, stride=2)
 
     def get_compressed_vec(self, features: torch.Tensor) -> torch.Tensor:
-        return self.get_internal_vec(features).view(-1, self.compressed_dim)
+        return self.get_internal_vec(features)
 
     def get_internal_vec(self, features: torch.Tensor) -> torch.Tensor:
         x = self.relu(self.conv1(features))
         x = self.pool(x)
         x = self.relu(self.conv2(x))
         x = self.pool(x)
+        x = x.reshape(-1, self.num_before_compressed_dim)
+        x = self.linear_encoder(x)
+        x = self.relu(x)
         return x
 
     def forward(self, features: torch.Tensor) -> torch.Tensor:
         code = self.get_internal_vec(features)
-        x = self.relu(self.t_conv1(code))
+        x = self.linear_decoder(code)
+        x = self.relu(x)
+        x = x.reshape(-1, *self.before_compressed_dim)
+        x = self.relu(self.t_conv1(x))
         x = self.relu(self.t_conv2(x))
         return x
